e827d52b377bec2d2eb201fa6f4e26d27c637591
leandro-dev-arg <121953105+leandro-dev-arg@users.noreply.github.com>
2023-12-27 17:05:18 -0300

    Add files via upload

---
+++ ejercicio 1 guia 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{int n1,n2,r;
+	cout<<"hola, ingresa un numero por favor..."<<endl;
+	cin>>n1;
+
+	cout<<"ingresa otro numero por favor..."<<endl;
+	cin>>n2;
+	r=n1+n2;
+	cout<<endl<<"el resultado es "<<r<<endl;
+	return 0;
+
+}
